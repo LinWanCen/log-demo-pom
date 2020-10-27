3a9a6e7ab31f608871ccd48b7a7632b388c5bae3
林万程 <1498425439@qq.com>
2020-10-27 23:06:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# 日志配置 Log Configuration
+
+## 配置文件 Configuration File
+
+[log4j2.xml](log4j2-demo/src/main/resources/log4j2.xml)
+
+[logback.xml](logback-demo/src/main/resources/logback.xml)
+
+
+## 演示程序 Demo Program
+
+[Log4j2Demo.java](log4j2-demo/src/main/java/io/github/linwancen/demo/log4j2/Log4j2Demo.java)
+
+[logback.java](logback-demo/src/main/java/io/github/linwancen/demo/logback/LogbackDemo.java)
+
+
+## 官方文档参考 Documentation
+
+[log4j2](http://logging.apache.org/log4j/2.x/manual/appenders.html)
+
+[logback](http://logback.qos.ch/manual/appenders.html)
+
+[slf4j](http://www.slf4j.org/faq.html#yet_another_facade)
---
+++ log4j2-demo/src/main/java/io/github/linwancen/demo/log4j2/Log4j2Demo.java
@@ -0,0 +1,31 @@
+package io.github.linwancen.demo.log4j2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.UUID;
+
+public class Log4j2Demo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Log4j2Demo.class);
+    private static final Logger LOGGER_OTHER = LoggerFactory.getLogger("other");
+
+    public static void main(String[] args) {
+        String traceId = UUID.randomUUID().toString().replace("-", "");
+        MDC.put("traceId", traceId);
+        int i = 0;
+        do {
+            LOGGER.info("中文正常 生僻字：爨 LOGGER.info {}", i);
+            LOGGER.warn("中文正常 生僻字：爨 LOGGER.warn {}", i);
+            LOGGER.error("中文正常 生僻字：爨 LOGGER.error {}", i);
+            LOGGER_OTHER.info("中文正常 生僻字：爨 LOGGER_OTHER.info {}", i);
+            LOGGER_OTHER.warn("中文正常 生僻字：爨 LOGGER_OTHER.warn {}", i);
+            LOGGER_OTHER.error("中文正常 生僻字：爨 LOGGER_OTHER.error {}", i);
+            i++;
+        }
+        // while (true);
+        while (i < 1_000_000);
+        // while (i < 1);
+    }
+}
